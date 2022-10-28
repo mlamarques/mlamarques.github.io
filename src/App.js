65edcb2dcd881d5adeadaa2aa2b07c0be46892ac
Mark Alves <lamarques.marcos@gmail.com>
2022-10-28 03:01:18 -0300

    Create IntersectionObserver

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
+import { useEffect, useRef, useState } from 'react'
 import './App.css';
 
 export default function App() {
-
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      console.log(entry);
-      if (entry.isIntersecting) {
-        entry.target.classList.add('show');
-      } else {
-        entry.target.classList.remove('show');
-      }
-    });
-  });
-
-  const hiddenElements = document.querySelectorAll('.hidden');
-  hiddenElements.forEach((element) => observer.observe(element));
+  const ref = useRef(null)
+  const [isVisible, setIsVisible] = useState(false)
+  console.log('isVisible: ', isVisible)
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const [entry] = entries
+      setIsVisible(entry.isIntersecting)
+    })
+    observer.observe(ref.current)
+  }, [])
 
   return (
     <div className="App">
       <h1>Hi there</h1>
 
-      <section className="hidden">
+      <section className="show">
         <h1>Hi mom</h1>
         <p>This is a website</p>
       </section>
 
-      <section className="hidden">
-        <h2>These are some projects</h2>
-        <p>
-          The things you end up owning you. Its ok after losing end asd you will
-          know what you got back
-        </p>
+      <section ref={ref} className={isVisible ? 'show' : 'hidden'}>
+        <div >
+          <h2>These are some projects</h2>
+          <p>
+            The things you end up owning you. Its ok after losing end asd you will
+            know what you got back
+          </p>
+        </div>
+        
       </section>
 
-      <section className="hidden">
+      <section className='hidden'>
         <h2>Its ok to be ok</h2>
       </section>
     </div>
