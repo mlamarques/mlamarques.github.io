48dfaadb58e456f20e2c3dc6a9a66e77d96f1603
Mark Alves <lamarques.marcos@gmail.com>
2022-10-28 03:22:48 -0300

    Use react-intersection-observer hook

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import { useEffect, useRef, useState } from 'react'
+import { useInView } from 'react-intersection-observer';
 import './App.css';
 
 export default function App() {
-  const ref = useRef(null)
-  const [isVisible, setIsVisible] = useState(false)
+  const [ref, isVisible] = useInView()
+  const [refOk, isOkVisible] = useInView()
   console.log('isVisible: ', isVisible)
-  useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      const [entry] = entries
-      setIsVisible(entry.isIntersecting)
-    })
-    observer.observe(ref.current)
-  }, [])
 
   return (
     <div className="App">
@@ -22,8 +15,8 @@ export default function App() {
         <p>This is a website</p>
       </section>
 
-      <section ref={ref} className={isVisible ? 'show' : 'hidden'}>
-        <div >
+      <section >
+        <div ref={ref} className={isVisible ? 'show' : 'hidden'}>
           <h2>These are some projects</h2>
           <p>
             The things you end up owning you. Its ok after losing end asd you will
@@ -33,7 +26,7 @@ export default function App() {
         
       </section>
 
-      <section className='hidden'>
+      <section ref={refOk} className={isOkVisible ? 'show' : 'hidden'}>
         <h2>Its ok to be ok</h2>
       </section>
     </div>
